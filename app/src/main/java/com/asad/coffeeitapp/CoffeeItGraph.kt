ade6a12d53a15373f38fba53bafc41a0c9fc014c
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:32:54 +0430

    chore: Added CoffeeItGraph to manage navigation

--- app/src/main/java/com/asad/coffeeitapp/CoffeeItGraph.kt
+++ app/src/main/java/com/asad/coffeeitapp/CoffeeItGraph.kt
@@ -1,4 +1,15 @@
 package com.asad.coffeeitapp
 
-class CoffeeItGraph {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun CoffeeItGraph() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
+        composable(route = Screen.SplashScreen.route) {
+        }
+    }
 }
