21180e6e1564779eae6e588a713dd05c105a3340
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:00:25 +0430

    chore: Added SubSelectionResponseMapper

--- app/src/main/java/com/asad/coffeeitapp/data/mapper/SubSelectionResponseMapper.kt
+++ app/src/main/java/com/asad/coffeeitapp/data/mapper/SubSelectionResponseMapper.kt
@@ -1,4 +1,21 @@
 package com.asad.coffeeitapp.data.mapper
 
-class SubSelectionResponseMapper {
+import com.asad.coffeeitapp.core.ResponseMapper
+import com.asad.coffeeitapp.data.dataSource.remote.model.SubSelectionResponseModel
+import com.asad.coffeeitapp.domain.model.SubSelectionModel
+import javax.inject.Inject
+
+class SubSelectionResponseMapper @Inject constructor() :
+    ResponseMapper<SubSelectionModel, SubSelectionResponseModel> {
+    override fun mapToModel(model: SubSelectionResponseModel): SubSelectionModel {
+        return with(model) {
+            SubSelectionModel(id, name)
+        }
+    }
+
+    override fun mapFromModel(model: SubSelectionModel): SubSelectionResponseModel {
+        return with(model) {
+            SubSelectionResponseModel(id, name)
+        }
+    }
 }
