7048266d41f9c55d04967e04e231a221f3b37b8b
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-06 22:15:49 +0430

    chore: Added CoffeeMachineRepository

--- app/src/main/java/com/asad/coffeeitapp/domain/repository/CoffeeMachineRepository.kt
+++ app/src/main/java/com/asad/coffeeitapp/domain/repository/CoffeeMachineRepository.kt
@@ -1,4 +1,7 @@
 package com.asad.coffeeitapp.domain.repository
 
+import com.asad.coffeeitapp.domain.model.CoffeeMachineModel
+
 interface CoffeeMachineRepository {
+    suspend fun fetchCoffeeMachineInfo(id: String): Result<CoffeeMachineModel>
 }
