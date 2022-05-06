4c0649ca6b542c23364b344bf0bf5ca230cb2357
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:17:53 +0430

    chore: Binded caffeeMachineRemoteDataSource to its implementataion for hilt, and coffeeMachineRepository to its implementation

--- app/src/main/java/com/asad/coffeeitapp/di/CoffeeMachineBinderModule.kt
+++ app/src/main/java/com/asad/coffeeitapp/di/CoffeeMachineBinderModule.kt
@@ -2,6 +2,10 @@ package com.asad.coffeeitapp.di
 
 import com.asad.coffeeitapp.data.dataSource.local.CoffeeMachineLocalDataSource
 import com.asad.coffeeitapp.data.dataSource.local.CoffeeMachineLocalDataSourceImpl
+import com.asad.coffeeitapp.data.dataSource.remote.CoffeeMachineRemoteDataSource
+import com.asad.coffeeitapp.data.dataSource.remote.CoffeeMachineRemoteDataSourceImpl
+import com.asad.coffeeitapp.data.repository.CoffeeMachineRepositoryImpl
+import com.asad.coffeeitapp.domain.repository.CoffeeMachineRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,6 +18,9 @@ abstract class CoffeeMachineBinderModule {
     @Binds
     abstract fun bindCoffeeMachineLocalDS(coffeeMachineLocalDataSource: CoffeeMachineLocalDataSourceImpl): CoffeeMachineLocalDataSource
 
-//    @Binds
-//    abstract fun bindCoffeeMachineRemoteDS(coffeemachine)
+    @Binds
+    abstract fun bindCoffeeMachineRemoteDS(coffeeMachineRemoteDataSource: CoffeeMachineRemoteDataSourceImpl): CoffeeMachineRemoteDataSource
+
+    @Binds
+    abstract fun bindCoffeeMachineRepository(coffeeMachineRepository: CoffeeMachineRepositoryImpl): CoffeeMachineRepository
 }
