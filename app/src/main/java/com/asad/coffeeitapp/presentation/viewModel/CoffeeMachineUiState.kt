3e07d71738c138977c99e240d647a919150ff2fc
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:11:19 +0430

    chore: Added CoffeeMachineUiState data class

--- app/src/main/java/com/asad/coffeeitapp/presentation/viewModel/CoffeeMachineUiState.kt
+++ app/src/main/java/com/asad/coffeeitapp/presentation/viewModel/CoffeeMachineUiState.kt
@@ -1,4 +1,8 @@
 package com.asad.coffeeitapp.presentation.viewModel
 
-class CoffeeMachineUiState {
-}
+import com.asad.coffeeitapp.domain.model.CoffeeMachineModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class CoffeeMachineUiState(
+    val coffeeMachine: MutableStateFlow<CoffeeMachineModel>,
+)
