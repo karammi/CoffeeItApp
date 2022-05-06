552a45b81386aff2b63282a0a61cffd60c045711
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-06 23:50:34 +0430

    chore: Added CoffeeMachineResponseMapper

--- app/src/main/java/com/asad/coffeeitapp/data/mapper/CoffeeMachineResponseMapper.kt
+++ app/src/main/java/com/asad/coffeeitapp/data/mapper/CoffeeMachineResponseMapper.kt
@@ -1,4 +1,17 @@
 package com.asad.coffeeitapp.data.mapper
 
-class CoffeeMachineResponseMapper {
+import com.asad.coffeeitapp.core.ResponseMapper
+import com.asad.coffeeitapp.data.dataSource.remote.model.CoffeeMachineResponseModel
+import com.asad.coffeeitapp.domain.model.CoffeeMachineModel
+import javax.inject.Inject
+
+class CoffeeMachineResponseMapper @Inject constructor() :
+    ResponseMapper<CoffeeMachineModel, CoffeeMachineResponseModel> {
+    override fun mapToModel(model: CoffeeMachineResponseModel): CoffeeMachineModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun mapFromModel(model: CoffeeMachineModel): CoffeeMachineResponseModel {
+        TODO("Not yet implemented")
+    }
 }
