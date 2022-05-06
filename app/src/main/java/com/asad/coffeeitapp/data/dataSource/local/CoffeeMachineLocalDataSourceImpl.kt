974e2240646a3cae745e55fdf3ab67e9aacb74a4
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-06 22:21:27 +0430

    chore: Added VeiwModelScoped annotaion to CoffeeMachineLocalDataSourceImpl

--- app/src/main/java/com/asad/coffeeitapp/data/dataSource/local/CoffeeMachineLocalDataSourceImpl.kt
+++ app/src/main/java/com/asad/coffeeitapp/data/dataSource/local/CoffeeMachineLocalDataSourceImpl.kt
@@ -1,8 +1,10 @@
 package com.asad.coffeeitapp.data.dataSource.local
 
 import com.asad.coffeeitapp.core.db.CoffeeItDao
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
+@ViewModelScoped
 class CoffeeMachineLocalDataSourceImpl @Inject constructor(
     private val dao: CoffeeItDao,
 ) : CoffeeMachineLocalDataSource
