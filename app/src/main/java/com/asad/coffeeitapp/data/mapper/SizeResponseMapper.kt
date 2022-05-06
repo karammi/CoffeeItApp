a6c22154ac571a043e53eb915dace7bb58f6beb2
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:00:47 +0430

    chore: Added SizeResponseMapper

--- app/src/main/java/com/asad/coffeeitapp/data/mapper/SizeResponseMapper.kt
+++ app/src/main/java/com/asad/coffeeitapp/data/mapper/SizeResponseMapper.kt
@@ -1,4 +1,20 @@
 package com.asad.coffeeitapp.data.mapper
 
-class SizeResponseMapper {
+import com.asad.coffeeitapp.core.ResponseMapper
+import com.asad.coffeeitapp.data.dataSource.remote.model.SizeResponseModel
+import com.asad.coffeeitapp.domain.model.SizeModel
+import javax.inject.Inject
+
+class SizeResponseMapper @Inject constructor() : ResponseMapper<SizeModel, SizeResponseModel> {
+    override fun mapToModel(model: SizeResponseModel): SizeModel {
+        return with(model) {
+            SizeModel(id, name, v)
+        }
+    }
+
+    override fun mapFromModel(model: SizeModel): SizeResponseModel {
+        return with(model) {
+            SizeResponseModel(id, name, v)
+        }
+    }
 }
