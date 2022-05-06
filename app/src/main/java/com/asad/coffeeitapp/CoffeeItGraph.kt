c1295439e762ece8cf7a5eff048a74c37c6cf68b
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 00:37:58 +0430

    chore: Added CoffeeItGraph to mainActivity setContent

--- app/src/main/java/com/asad/coffeeitapp/CoffeeItGraph.kt
+++ app/src/main/java/com/asad/coffeeitapp/CoffeeItGraph.kt
@@ -4,12 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.asad.coffeeitapp.splash.screen.SplashScreen
 
 @Composable
 fun CoffeeItGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
         composable(route = Screen.SplashScreen.route) {
+            SplashScreen(navController = navController)
         }
     }
 }
