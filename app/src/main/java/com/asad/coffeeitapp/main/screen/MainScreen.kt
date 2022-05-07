deb60229e60f0734cfe7ad3fd1cac311d2778bc7
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 10:43:29 +0430

    chore: Added MainScreen

--- app/src/main/java/com/asad/coffeeitapp/main/screen/MainScreen.kt
+++ app/src/main/java/com/asad/coffeeitapp/main/screen/MainScreen.kt
@@ -1,2 +1,9 @@
 package com.asad.coffeeitapp.main.screen
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController,) {
+
+}
