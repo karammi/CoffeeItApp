23c0fa3c00e83d8546dbd2f959a061547c0a15a8
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-05-07 10:44:01 +0430

    chore: Added Navigation to MainScreen in SplashScreen

--- app/src/main/java/com/asad/coffeeitapp/splash/screen/SplashScreen.kt
+++ app/src/main/java/com/asad/coffeeitapp/splash/screen/SplashScreen.kt
@@ -1,6 +1,7 @@
 package com.asad.coffeeitapp.splash.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
@@ -10,14 +11,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.asad.coffeeitapp.R
+import com.asad.coffeeitapp.Screen
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    SplashScreenContent()
+    SplashScreenContent(navController)
 }
 
 @Composable
-fun SplashScreenContent() {
+fun SplashScreenContent(navController: NavController) {
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
             Text(text = "Dark Roasted Beans")
@@ -27,6 +29,9 @@ fun SplashScreenContent() {
                 contentDescription = "device",
                 modifier = Modifier
                     .fillMaxSize()
+                    .clickable {
+                        navController.navigate(Screen.MainScreen.route)
+                    }
             )
         }
     }
